576e8bf0a2bb7b4335aa2d1e20709149e15cc177
amitavainfluxiq <amitava.involutiontech@gmail.com>
2019-05-27 15:46:47 +0530

    change in contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormBuilder, Validators} from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,7 +9,7 @@ import {FormControl, FormBuilder, Validators} from '@angular/forms';
 export class ContactComponent implements OnInit {
 public myform: any;
 public data: any;
-  constructor( public formbuilder: FormBuilder) {
+  constructor( public formbuilder: FormBuilder, public _http:HttpClient) {
     this.myform = this.formbuilder.group({
       email: ['', Validators.compose([Validators.required, Validators.pattern(/^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)])],
       fname: ['',Validators.required],
@@ -31,9 +31,21 @@ public data: any;
     for(let i in this.myform.controls){
       this.myform.controls[i].markAsTouched();
     }
-   /* if(this.myform.valid){
+    if(this.myform.valid){
+      let link = 'http://192.169.196.208:7051/contactUs_applicant';
+      let data = {data:this.myform.value};
+      this._http.post(link,data)
+          .subscribe(res=>{
 
-    }*/
+            let result:any = {};
+            result = res;
+            console.log(result);
+            if(result.status== 'success'){
+              //this.myform.reset();
+            }
+          })
+
+    }
 
   }
   inputUntouch(form:any,val:any){
